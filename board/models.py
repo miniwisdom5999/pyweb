8c2a9b67979353c4a1243f34fcf974dbbebe1be8
miniwisdom5999 <miniwisdom@naver.com>
2021-12-27 12:59:18 +0900

    index, polls페이지

--- board/models.py
+++ board/models.py
@@ -1,14 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)  #제목 칼럼
     content = models.TextField()                #질문 내용
     create_date = models.DateTimeField()        #질문 작성일
+    modify_date = models.DateTimeField(null=True, blank=True)  # 질문 수정일
+
 
     def __str__(self):
         return self.subject
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #외래키, 제목
     content = models.TextField()          #답변 내용
     create_date = models.DateTimeField()  #답변 작성일
