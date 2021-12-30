d8ff0634595e7e82662efd0f8d7becc79557e577
miniwisdom5999 <miniwisdom@naver.com>
2021-12-30 10:54:55 +0900

    검색 기능 추가

--- board/urls.py
+++ board/urls.py
@@ -39,11 +39,11 @@ urlpatterns = [
          name='comment_create_question'),
 
     # 질문 댓글 삭제
-    path('comment/delete/question/<int:question_id>/', views.comment_delete_question,
+    path('comment/delete/question/<int:comment_id>/', views.comment_delete_question,
          name='comment_delete_question'),
 
     # 질문 댓글 수정
-    path('comment/modify/question/<int:question_id>/', views.comment_modify_question,
+    path('comment/modify/question/<int:comment_id>/', views.comment_modify_question,
          name='comment_modify_question'),
 
 
--- board/views.py
+++ board/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -8,24 +9,31 @@ from django.utils import timezone
 from board.models import Question, Answer, Comment
 from board.forms import QuestionForm, AnswerForm, CommentForm
 
-
 def index(request):
     return render(request, 'board/index.html')
-
 
 def boardlist(request):
     #질문 목록
     #question_list = Question.objects.all()  #db 전체조회
-    question_list = Question.objects.order_by('-create_date')   #작성일 기준 내림차순(- 기호 사용)
 
-    #페이지 처리
-    page = request.GET.get('page', 1)   #127.0.0.1:8000/ 기본 1페이지임
-    paginator = Paginator(question_list, 10)    #페이지당 10개씩 설정
-    page_obj = paginator.get_page(page) #페이지 가져오기
+    page = request.GET.get('page', 1)  #127.0.0.1:8000/ 기본 1페이지임
+    kw = request.GET.get('kw', '')     #검색어 가져오기
 
-    return render(request, 'board/question_list.html',
-                  {'question_list':page_obj})
-    #return HttpResponse("pyweb 사이트 입니다.")
+    # 조회
+    question_list = Question.objects.order_by('-create_date')  # 작성일 내림차순
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이
+            Q(answer__author__username__icontains=kw) |   # 답변 글쓴이
+            Q(answer__content__icontains=kw)
+        ).distinct()   #유일한 것 검색
+
+    paginator = Paginator(question_list, 10) #페이지당 10개씩 설정
+    page_obj = paginator.get_page(page)    #페이지 가져오기
+    context = {'question_list':page_obj, 'page':page, 'kw':kw}
+    return render(request, 'board/question_list.html', context)
 
 def detail(request, question_id):
     # 질문/답변 상세
@@ -44,7 +52,7 @@ def question_create(request):
             question.create_date = timezone.now() #날짜 시간 저장
             question.author = request.user  #글쓴이에 세션 저장
             question.save()  #실제 저장
-            return redirect('board:index') #이동할 경로(앱 네임사용) 저장
+            return redirect('board:boardlist') #이동할 경로(앱 네임사용) 저장
     else:
         form = QuestionForm()   #form 객체 생성
     return render(request, 'board/question_form.html', {'form':form})
@@ -85,27 +93,27 @@ def question_modify(request, question_id):
     return render(request, 'board/question_form.html', {'form':form})
 
 @login_required(login_url='common:login')
-def question_delete(request, question_id):
-    #질문 삭제
-    question = get_object_or_404(Question, pk=question_id)
-    question.delete() #질문 삭제
-    return redirect('board:index')
-
-@login_required(login_url='common:login')
 def answer_modify(request, answer_id):
     #답변 수정
     answer = get_object_or_404(Answer, pk=answer_id)
     if request.method == "POST":
         form = AnswerForm(request.POST, instance=answer)
         if form.is_valid():
-            answer = form.save(commit=False)  #수정된 답변 가저장
-            answer.author = request.user      #세션 발급
-            answer.modify_date = timezone.now() #수정일
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.author = request.user
             answer.save()
             return redirect('board:detail', question_id=answer.question.id)
     else:
-        form = AnswerForm(instance=answer)  #instance를 쓰면 폼에 내용이 채워짐
+        form = AnswerForm(instance=answer)
     return render(request, 'board/answer_form.html', {'form':form})
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    #질문 삭제
+    question = get_object_or_404(Question, pk=question_id)
+    question.delete() #질문 삭제
+    return redirect('board:index')
 
 @login_required(login_url='common:login')
 def answer_delete(request, answer_id):
@@ -114,39 +122,33 @@ def answer_delete(request, answer_id):
     answer.delete()
     return redirect('board:detail', question_id=answer.question.id)
 
-
-
 @login_required(login_url='common:login')
 def vote_question(request, question_id):
     #질문 추천
     question = get_object_or_404(Question, pk=question_id)
     if request.user == question.author:
-        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다')
+        messages.error(request, '본인이 작성한 글은 추천할 수 없습니다.')
     else:
-        question.voter.add(request.user)    #추천 추가(로그인한 사람)
+        question.voter.add(request.user)  #추천 추가(로그인한 사람)
     return redirect('board:detail', question_id=question.id)
-
 
 @login_required(login_url='common:login')
 def comment_create_question(request, question_id):
-    #질문 댓글 등록
+    # 질문 댓글 등록
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
-        form = CommentForm(request.POST)    #입력된 댓글 내용
+        form = CommentForm(request.POST)  #입력된 댓글 내용
         if form.is_valid():
-            comment = form.save()
-            comment.author = request.user   #세션권한
+            comment = form.save(commit=False)
+            comment.author = request.user  #세션 권한
             comment.create_date = timezone.now()
-            comment.question = question  #참조 외래키
-            comment.save()      #실제 저장
+            comment.question = question   #참조 외래키
+            comment.save()   # 실제 저장
             return redirect('board:detail', question_id=question.id)
-
-    else:   # GET은 빈 폼을 가져옴
-        form =CommentForm()
+    else: #GET은 빈 폼을 가져옴
+        form = CommentForm()
     context = {'form':form}
     return render(request, 'board/comment_form.html', context)
-
-
 
 @login_required(login_url='common:login')
 def comment_delete_question(request, comment_id):
@@ -155,22 +157,19 @@ def comment_delete_question(request, comment_id):
     comment.delete()
     return redirect('board:detail', question_id=comment.question.id)
 
-
-
 @login_required(login_url='common:login')
 def comment_modify_question(request, comment_id):
     # 질문 댓글 수정
     comment = get_object_or_404(Comment, pk=comment_id)
     if request.method == "POST":
-        form = CommentForm(request.POST, instance=comment)  #변경된 입력내용
+        form = CommentForm(request.POST, instance=comment) #변경된 입력 내용
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.author = request.uer
-            comment.mpdify_date = timezone.now()
+            comment.author = request.user
+            comment.modify_date = timezone.now()
             comment.save()
             return redirect('board:detail', question_id=comment.question.id)
     else:
-        form = CommentForm(instance=comment)    #채워진 폼
+        form = CommentForm(instance=comment)  # 채워진 폼
     context = {'form':form}
     return render(request, 'board/comment_form.html', context)
-
